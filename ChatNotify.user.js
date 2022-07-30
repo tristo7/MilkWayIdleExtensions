3e21625b08529e2d85f9f4fd222669ca3620db7f
Tristin Terry <tristo7000@gmail.com>
2022-07-30 10:01:34 -0500

    initial implementation for whispers

---
+++ ChatNotify.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         Chat Notify
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Notify chat messages
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=milkywayidle.com
+// @author       tristo7
+// @updateURL    https://github.com/tristo7/MilkyWayIdleExtensions/raw/main/ChatNotify.user.js
+// @downloadURL  https://github.com/tristo7/MilkyWayIdleExtensions/raw/main/ChatNotify.user.js
+// @match        *://*.milkywayidle.com/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+const nativeWebSocket = window.WebSocket;
+window.WebSocket = function(...args){
+  const socket = new nativeWebSocket(...args);
+  window.cowsocket = socket;
+  window.cowsocket.addEventListener('message', function handler(e){
+    let msg = JSON.parse(e.data);
+    if( msg.type == 'init_character_info' ){
+      window.init_character_info = msg;
+    }
+    if(typeof window.init_character_info !== 'undefined'){
+        window.cowsocketready = true;
+        console.log(`Attached to WebSocket.`);
+        this.removeEventListener('message', handler);
+    }
+  });
+  return socket;
+};
+
+const addListener = (action) => {
+    window.cowsocket.addEventListener('message', action);
+};
+
+const interceptChat = (msg) => {
+    const data = JSON.parse(msg.data);
+
+    const { type, message } = data;
+    if (type !== 'chat_message_received') {
+        return;
+    }
+
+    const { senderName, receiverName, channelTypeHrid } = message;
+
+    if (channelTypeHrid === '/chat_channel_types/whisper' && receiverName === window.init_character_info.user.username){
+        displayMessage(`New whisper from ${senderName}`)
+    }
+};
+
+const displayMessage = (message) => {
+    let note = new Notification( message, {icon: "/assets/images/logos/logo_transparent_570.svg"} );
+    note.onclick = function(){window.focus(); this.close()};
+    setTimeout(()=>{note.close()}, 10*1000);
+  }
+
+(async function() {
+    'use strict';
+    
+    while (!window.cowsocketready) {
+        await new Promise(r => setTimeout(r, 250));
+    }
+
+    Notification.requestPermission();
+
+    window.cowsocket.addEventListener('message', interceptChat);
+})();
